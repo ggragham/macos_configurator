ad8e4278139d59b069107526a5b3b9d9d7997abc
Grell Gragham <vrt.epm@protonmail.com>
2022-10-02 01:48:48 +0300

    feat: add finder configs

--- scripts/config_base.sh
+++ scripts/config_base.sh
@@ -2,3 +2,9 @@
 
 # Set visible files extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder quiet
+defaults write com.apple.finder "QuitMenuItem" -bool "true"
+
+# Path bar
+defaults write com.apple.finder "ShowPathbar" -bool "true"
