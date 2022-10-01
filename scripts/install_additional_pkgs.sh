3454067e374d5822fc6240266273ff3fe4afabe7
Grell Gragham <vrt.epm@protonmail.com>
2022-10-02 00:50:37 +0300

    feat: update additional pkgs

--- scripts/install_additional_pkgs.sh
+++ scripts/install_additional_pkgs.sh
@@ -6,6 +6,9 @@ trap 'errMsg' ERR
 cd "$(dirname "$0")" || exit "$?"
 
 PKG_LIST_PATH="../pkgs"
+HOME_PATH="/Users/$USER"
+LOCAL_PATH="$HOME_PATH/.local"
+BIN_PATH="$LOCAL_PATH/bin"
 
 errMsg() {
     echo "Failed"
@@ -36,9 +39,25 @@ installAdditionalPkgs() {
     fi
 }
 
+downloadScritps() {
+    local pkgsFile=""
+    pkgsFile="$(cat "$PKG_LIST_PATH/scripts.pkgs")"
+    IFS=$'\n'
+
+    for scriptName in $pkgsFile; do
+        echo "Downloading $scriptName"
+        curl "https://raw.githubusercontent.com/ggragham/just_bunch_of_scripts/master/bin/$scriptName" -o "$BIN_PATH/$scriptName"
+        chmod +x "$BIN_PATH/$scriptName"
+    done
+
+    echo "Scripts has been downloaded"
+}
 main() {
     doNotRunAsRoot
+
     installAdditionalPkgs
+    downloadScritps
+
     pressAnyKeyToContinue
 }
 
