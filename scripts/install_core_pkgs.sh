b9ae5b6b63b608efa8dd2195ee69f84a0d44221b
Grell Gragham <vrt.epm@protonmail.com>
2023-10-04 02:10:01 +0300

    feat: [ansible] add brew pkgs installation

--- scripts/install_core_pkgs.sh
+++ scripts/install_core_pkgs.sh
@@ -29,60 +29,6 @@ pressAnyKeyToContinue() {
     echo
 }
 
-installOMZ() {
-    local omzDownloadURL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
-    curl "$omzDownloadURL" -o "$OPT_PATH/install.sh"
-    export ZSH=$OPT_PATH/oh-my-zsh
-    export CHSH=no
-    export RUNZSH=no
-    if [[ -f "$OPT_PATH/install.sh" ]]; then
-        bash "$OPT_PATH/install.sh"
-        echo "Oh My Zsh has been installed"
-    else
-        local errcode="$?"
-        echo "Failed to install Oh My Zsh"
-        pressAnyKeyToContinue
-        exit "$errcode"
-    fi
-
-}
-
-installOMZPlugins() {
-    local pluginDir="$ZSH/custom/plugins"
-    if [[ -d $pluginDir ]]; then
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$pluginDir/zsh-syntax-highlighting"
-        git clone https://github.com/zsh-users/zsh-autosuggestions.git "$pluginDir/zsh-autosuggestions"
-        echo "Plugins for Oh My Zsh have been installed"
-    else
-        echo "Failed to install plugins for Oh My Zsh"
-        pressAnyKeyToContinue
-        exit 1
-    fi
-}
-
-applyOMZConfig() {
-    local omzConfig="$CONFIG_PATH/omz"
-    if cp -rf "$omzConfig/." "$HOME"; then
-        echo "OMZ config has been loaded"
-    else
-        local errcode="$?"
-        echo "Failed to load OMZ config"
-        pressAnyKeyToContinue
-        exit "$errcode"
-    fi
-}
-
-installCorePkgs() {
-    local brewfile="$PKG_LIST_PATH/core.brewfile"
-    if brew bundle --file="$brewfile"; then
-        echo "Brew pkgs has been installed"
-    else
-        local errcode="$?"
-        echo "Failed to install brew pkgs"
-        pressAnyKeyToContinue
-        exit "$errcode"
-    fi
-}
 
 applyLocalConfigs() {
     applyConfigs() { (
@@ -105,10 +51,6 @@ applyLocalConfigs() {
 main() {
     doNotRunAsRoot
 
-    installOMZ
-    installOMZPlugins
-    applyOMZConfig
-    installCorePkgs
     applyLocalConfigs
 
     pressAnyKeyToContinue
