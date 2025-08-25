851760bf224fca6ab135ba69ba6f7166e8cbc6f4
Grell Gragham <vrt.epm@protonmail.com>
2025-08-25 21:45:31 +0300

    feat: remove fanny config

--- scripts/plists/com.fannywidget.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#########
-# Fanny #
-#########
-
-defaults write "$HOME"/Library/Group\ Containers/fanny-shared-defaults/Library/Preferences/fanny-shared-defaults.plist FNYUserPreferencesKey_MenuBarIconOption 1
